2baff7adce51db72825802b00092bf4c1c9b5654
NIGHT9910 <155859895+NIGHT9910@users.noreply.github.com>
2024-02-12 20:28:05 +0100

    Add files via upload

---
+++ index.js
@@ -0,0 +1,5 @@
+var animenumber1 = Math.floor(Math.random() * 94) + 1 ;
+
+var animeluck= "pictures/anime" + animenumber1 + ".jpg" ;
+
+document.querySelectorAll("img")[0].setAttribute("src", animeluck) ;
